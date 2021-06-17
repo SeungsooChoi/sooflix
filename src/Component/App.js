ae5899da43bced59c2db2c654f30fb2e2e1b7971
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-06-17 16:50:45 +0900

    Feat : add Header, Switch

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Router from "Component/Router";
+import Header from "Component/Header";
 
 class App extends React.Component {
   render() {
     return (
       <>
+        <Header />
         <Router />
       </>
     );
