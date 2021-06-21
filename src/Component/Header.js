6fa002c9769ab6e8c30a9ebedea39a95998e0eb8
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-06-21 16:06:18 +0900

    Feat : Get location.pathname with withRouter

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 
 const Header = styled.header`
@@ -24,6 +24,9 @@ const Item = styled.li`
   width: 80px;
   height: 50px;
   text-align: center;
+  border-bottom: 3px solid
+    ${(props) => (props.current ? "#3498db" : "transparent")};
+  transition: border-bottom 0.5s ease-in-out;
 `;
 
 const SLink = styled(Link)`
@@ -33,18 +36,21 @@ const SLink = styled(Link)`
   justify-content: center;
 `;
 
-export default () => (
+// withRouter는 다른 컴포넌트를 감싸는 컴포넌트.
+// 그리고 Router에 정보를 줌
+// props에 접근 할 수 있다.
+export default withRouter(({ location: { pathname } }) => (
   <Header>
     <List>
-      <Item>
+      <Item current={pathname === "/"}>
         <SLink to="/">Movies</SLink>
       </Item>
-      <Item>
+      <Item current={pathname === "/tv"}>
         <SLink to="/tv">TV</SLink>
       </Item>
-      <Item>
+      <Item current={pathname === "/search"}>
         <SLink to="/search">Search</SLink>
       </Item>
     </List>
   </Header>
-);
+));
