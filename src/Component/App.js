3a5fef2af5ce1305125c628a41639fb352a8e8ff
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-06-21 15:41:55 +0900

    Feat : Add styled-reset for GlobalStyles

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Router from "Component/Router";
+import GlobalStyles from "Component/GlobalStyles";
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Router />
+        <GlobalStyles />
       </>
     );
   }
