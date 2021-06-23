aee1369c023bc6491e625029e3f781f903edceb3
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-06-23 15:36:44 +0900

    Feat : HomePresenter and Section Components

--- src/Component/GlobalStyles.js
+++ src/Component/GlobalStyles.js
@@ -11,7 +11,7 @@ const globalStyles = createGlobalStyle`
         font-size:12px;
         color:white;
         background-color:rgba(20, 20, 20, 1);
-        padding-top: 50px;
+        padding-top: 80px;
     }
     a{
         text-decoration:none;
