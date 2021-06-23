13f988bb2b6a6a6f935636a7d856f89ef788b973
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-06-23 14:55:37 +0900

    Feat : DetailContainer에서 movie, tv 검색목록 가져옴

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,12 +1,20 @@
+import { moviesApi, tvApi } from "api";
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
 
 export default class extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true,
-  };
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname },
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/"),
+    };
+  }
 
   async componentDidMount() {
     const {
@@ -15,16 +23,32 @@ export default class extends React.Component {
       },
       history: { push },
     } = this.props;
+    const { isMovie } = this.state;
     const parseId = Number(id);
     if (isNaN(parseId)) {
       push("/");
       return;
     }
+    let result;
+    try {
+      if (isMovie) {
+        // movie 찾음
+        const request = await moviesApi.movieDetail(parseId);
+        result = request.data;
+      } else {
+        const request = await tvApi.tvDetail(parseId);
+        result = request.data;
+      }
+      console.log(result);
+    } catch (error) {
+      this.setState({ error: "Cant find anything." });
+    } finally {
+      this.setState({ loading: false, result });
+    }
   }
 
   render() {
     const { result, error, loading } = this.state;
-    console.log(this.props);
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
 }
