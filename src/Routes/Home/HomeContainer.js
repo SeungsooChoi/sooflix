486cd540423827383df82edf5a68d5e3547a63cb
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-06-23 14:08:04 +0900

    Feat : Add componentDidMount in HomeContainer

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HomePresenter from "./HomePresenter";
+import { moviesApi } from "api";
 
 export default class extends React.Component {
   state = {
@@ -9,10 +10,40 @@ export default class extends React.Component {
     error: null,
     loading: true,
   };
+  // 컴포넌트가 마운트 되었을 때 nowPlaying, upcoming, popular를 찾고 state 설정
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying },
+      } = await moviesApi.nowPlaying();
+
+      const {
+        data: { results: upcoming },
+      } = await moviesApi.upcoming();
+
+      const {
+        data: { results: popular },
+      } = await moviesApi.popular();
+
+      this.setState({
+        nowPlaying,
+        upcoming,
+        popular,
+      });
+    } catch (error) {
+      this.setState({ error: "Cant find movies information" });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
   // api 및 error 처리
 
   render() {
     const { nowPlaying, upcoming, popular, error, loading } = this.state;
+    console.log(this.state);
     return (
       <HomePresenter
         nowPlaying={nowPlaying}
