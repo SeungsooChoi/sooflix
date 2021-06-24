e12c5a9c68fc45cedacd731e798befe374990c05
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-06-24 20:12:19 +0900

    Feat : SearchPresenter Component

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -5,7 +5,7 @@ import Section from "Component/Section";
 import Loader from "Component/Loader";
 
 const Container = styled.div`
-  padding: 0px 10px;
+  padding: 0px 20px;
 `;
 
 const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
